0b791b8e67404ca9be0b2aa7c728b2e2c8e9d435
kavallo <luis.aneudy@gmail.com>
2021-09-29 12:27:28 -0400

    ....

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool autenticar(string user)
+        {
+            if(user == "user1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
